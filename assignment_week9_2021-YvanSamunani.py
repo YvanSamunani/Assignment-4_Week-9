638b09fd0e82d12762e965067fc25a38735a5027
Yvan Samunani <yvansamu@gmail.com>
2021-12-07 11:13:01 +0200

    First commit

--- assignment_week9_2021-YvanSamunani.py
+++ assignment_week9_2021-YvanSamunani.py
@@ -42,36 +42,3 @@ books_table.sort_values("Prices", axis= 0, ascending = True, inplace = True, na_
 
 # Printing the table with book names, reviews and their prices.
 print(books_table)
-
-
-
-
-
-
-
-
-
-import requests
-from bs4 import BeautifulSoup
-
-book_prices = []
-page = requests.get('https://www.amazon.com/Best-Sellers-Books/zgbs/books/ref=zg_bs_pg_1?_encoding=UTF8&pg=1')
-
-print(page)
-soup = BeautifulSoup(page.content, 'lxml')
-best_sellers = soup.find_all(class_='a-size-base a-color-price')
-
-
-for books in best_sellers:
-    new_string = float(books.text.replace("$", ""))
-    print(new_string)
-    book_prices.append(new_string)
-
-book_prices.sort(reverse=True)
-print(book_prices)
-
-
-
-# # print(soup.prettify())
-
-
